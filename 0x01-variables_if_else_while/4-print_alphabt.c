ff75d6f3512bc365ba030f5770a1f376a876f824
horluwathobiloba <dmbjanetkehinde@gmail.com>
2022-07-01 10:26:25 -0700

    Task 4

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,19 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/**
+ * main - Entry point
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char abc;
+
+	for (abc = 'a'; abc <= 'z'; abc++)
+	{
+		if ((abc != 'e') && (abc != 'q'))
+			putchar(abc);
+	}
+	putchar('\n');
+	return (0);
+}
