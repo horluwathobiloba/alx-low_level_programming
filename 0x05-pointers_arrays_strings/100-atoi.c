0afb00ce9557a039a368de216bc80d4a9564493c
horluwathobiloba <dmbjanetkehinde@gmail.com>
2022-07-12 14:10:26 -0700

    advance task

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -15,26 +15,22 @@ int _atoi(char *s)
 
 	while (s[c])
 	{
-		if (s[c] == 45)		
+		if (s[c] == 45)
 		{
 			min *= -1;
 		}
-
 		while (s[c] >= 48 && s[c] <= 57)
 		{
 			isi = 1;
 			ni = (ni * 10) + (s[c] - '0');
 			c++;
 		}
-
 		if (isi == 1)
 		{
-															break;
+			break;
 		}
-
 		c++;
 	}
-
 	ni *= min;
 	return (ni);
 }
